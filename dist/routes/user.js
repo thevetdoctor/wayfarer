f58709415b183ca930e0c4eb83b3b129066ebd67
oba <thevetdoctor@gmail.com>
2019-07-15 03:47:45 +0100

    chore(heroku):host on heroku
    [Finishes #167107326]

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -9,20 +9,19 @@ var _express = _interopRequireDefault(require("express"));
 
 var _user = _interopRequireDefault(require("../controllers/user"));
 
-var _validateUser = _interopRequireDefault(require("../helpers/validate/validateUser"));
-
 var _auth = _interopRequireDefault(require("../auth"));
 
 var _adminAuth = _interopRequireDefault(require("../auth/adminAuth"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var router = _express["default"].Router(); // router.post('/signup', UserController.createUser);
+// import validateUser from '../helpers/validate/validateUser';
+var router = _express["default"].Router();
 
+router.post('/signup', _user["default"].createUser); // router.post('/signup', validateUser.validateSignup, UserController.createUser);
 
-router.post('/signup', _validateUser["default"].validateSignup, _user["default"].createUser); // router.post('/signin', UserController.signIn);
+router.post('/signin', _user["default"].signIn); // router.post('/signin', validateUser.validateSignin, UserController.signIn);
 
-router.post('/signin', _validateUser["default"].validateSignin, _user["default"].signIn);
 router.get('/', _auth["default"], _adminAuth["default"], _user["default"].getUsers);
 var _default = router;
 exports["default"] = _default;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import UserController from '../controllers/user';
-import validateUser from '../helpers/validate/validateUser';
+// import validateUser from '../helpers/validate/validateUser';
 import auth from '../auth';
 import adminAuth from '../auth/adminAuth';
 
 const router = express.Router();
 
 
-// router.post('/signup', UserController.createUser);
-router.post('/signup', validateUser.validateSignup, UserController.createUser);
+router.post('/signup', UserController.createUser);
+// router.post('/signup', validateUser.validateSignup, UserController.createUser);
 
-// router.post('/signin', UserController.signIn);
-router.post('/signin', validateUser.validateSignin, UserController.signIn);
+router.post('/signin', UserController.signIn);
+// router.post('/signin', validateUser.validateSignin, UserController.signIn);
 
 router.get('/', auth, adminAuth, UserController.getUsers);
 
